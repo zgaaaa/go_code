35b4ee9c2c0de690205282d305a08dda6bc21f24
zgaaa <1924297895@qq.com>
2020-11-15 21:41:05 +0800

    master

--- main/main.go
+++ main/main.go
@@ -1,25 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Person 结构体Person
+type Person struct {
+	Name string
+}
+
+// 给结构体Person绑定一个方法
+func (p Person) name() {
+	fmt.Println(p.Name, "是一个好人")
+}
+
+func (p Person) jisuan(n int) {
+	sum := 0
+	for i := 1; i < n+1; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func (p Person) getsum(a, b int) {
+	fmt.Println(a + b)
+}
 
 func main() {
-	defer func() {
-		err := recover()
-		if err != nil {
-			fmt.Println("err = ", err)
-		}
-	}()
-	type Cat struct {
-		array1 [3]int
-		map1   map[string]string
-		slice1 []string
-	}
-	var cat1 Cat
-	// cat1.array1 = [3]int{1, 2, 3}
-	// cat1.slice1 = []string{"大毛"}
-	// cat1.map1 = map[string]string{
-	// 	"name":"二毛",
-	// }
-	cat1.map1["age"] = "4"
-	fmt.Println(cat1)
+	var p1 Person
+	p1.Name = "tom"
+	p1.jisuan(1000)
+	p1.getsum(10, 20)
 }
