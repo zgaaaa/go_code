b1e3a87742a1f9c32cd6819ac027da4854d6562e
zgaaa <1924297895@qq.com>
2021-01-01 16:27:48 +0800

    master

--- main/main.go
+++ main/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"go_code/demo"
+    "fmt"
+    "go_code/demo"
 )
 
 func main() {
-	acc := demo.Getaccount()
-	acc.Setzhanghao("123456")
-	fmt.Println((*acc).Getzhanghao())
+    acc := demo.Getaccount()
+    acc.Setzhanghao("123456")
+    fmt.Println((*acc).Getzhanghao())
 }
