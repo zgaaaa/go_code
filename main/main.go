9a0da40c0d45b9aee5e8cc040a9071548ac932b2
zgaaa <1924297895@qq.com>
2021-01-29 00:20:27 +0800

    master

--- main/main.go
+++ main/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "go_code/demo"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
-    acc := demo.Getaccount()
-    acc.Setzhanghao("123456")
-    fmt.Println((*acc).Getzhanghao())
+	file1 := "d:/go_work/github.com/zgaaaa/go_code/go.mod"
+	file2 := "d:/go_work/github.com/zgaaaa/go_code/text.txt"
+	data, err := ioutil.ReadFile(file1)
+	if err != nil {
+		fmt.Println("file1  ",err)
+	}
+	fmt.Printf("%s", data)
+	err = ioutil.WriteFile(file2,data,0777) // 覆盖写
+	if err != nil {
+		fmt.Println("写入错误 ",err)
+	}
 }
