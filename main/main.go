97afba3ca1310c64433dd6309b93ab407317ed9a
zgaaa <1924297895@qq.com>
2021-01-29 12:17:42 +0800

    master

--- main/main.go
+++ main/main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 )
 
 func main() {
-	file1 := "d:/go_work/github.com/zgaaaa/go_code/go.mod"
-	file2 := "d:/go_work/github.com/zgaaaa/go_code/text.txt"
-	data, err := ioutil.ReadFile(file1)
-	if err != nil {
-		fmt.Println("file1  ",err)
-	}
-	fmt.Printf("%s", data)
-	err = ioutil.WriteFile(file2,data,0777) // 覆盖写
-	if err != nil {
-		fmt.Println("写入错误 ",err)
-	}
+	
 }
