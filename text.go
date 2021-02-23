abeaafbdb0423482033e734aa46799521372292d
zgaaaa <1924297895@qq.com>
2021-02-24 03:46:23 +0800

    master

--- text.go
+++ text.go
@@ -5,17 +5,6 @@ import (
 )
 
 func main() {
-	slice1 := []int{1,2,3,4,5}
-	fmt.Println(runningSum(slice1))
-}
-func runningSum(nums []int) []int {
-    count := 0
-    var nums2 []int
-    nums2 = make([]int,len(nums))
-    for i := 0;i<len(nums);i++ {
-        count += nums[i]
-        nums2[i] = count
-        
-    }
-    return nums2
+	res := make([]int, 5)
+	fmt.Println(res)
 }
